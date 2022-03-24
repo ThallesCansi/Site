086a0d8f144b087e3e7ddb57366a160bbda7502d
Thalles Cansi <thalles_cansi@hotmail.com>
2022-03-23 21:50:16 -0300

    Mudanças na parte de comentários e estilo da navbar

---
+++ testimonials.js
@@ -0,0 +1,26 @@
+const testimonials = [
+    {
+        quote: "Nossa empresa, fazemos pesquisas contínuas com seu público-alvo. Isso inclui entrevistas telefônicas de 30 a 45 minutos. Foi difícil conduzir a entrevista, realmente ouvir e fazer boas perguntas de acompanhamento.",
+        avatar: "avatar1",
+        name: "Diana Ayi",
+        title: "Musician"
+    },
+    {
+        quote: "Nossa empresa, fazemos pesquisas contínuas com seu público-alvo. Isso inclui entrevistas telefônicas de 30 a 45 minutos. Foi difícil conduzir a entrevista, realmente ouvir e fazer boas perguntas de acompanhamento.",
+        avatar: "avatar2",
+        name: "Asamoah Gyan",
+        title: "Web Developer"
+    },
+    {
+        quote: "Nossa empresa, fazemos pesquisas contínuas com seu público-alvo. Isso inclui entrevistas telefônicas de 30 a 45 minutos. Foi difícil conduzir a entrevista, realmente ouvir e fazer boas perguntas de acompanhamento.",
+        avatar: "avatar3",
+        name: "Ernest Achiever",
+        title: "Footerballer"
+    },
+    {
+        quote: "Nossa empresa, fazemos pesquisas contínuas com seu público-alvo. Isso inclui entrevistas telefônicas de 30 a 45 minutos. Foi difícil conduzir a entrevista, realmente ouvir e fazer boas perguntas de acompanhamento.",
+        avatar: "avatar4",
+        name: "Beatrice Aku",
+        title: "Politician"
+    }
+]
